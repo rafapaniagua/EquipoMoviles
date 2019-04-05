6484259d0cdfd37104c007b5752ab87a4dd9f6a0
rafapaniagua <rafa_1_1@live.com.mx>
2019-04-04 21:48:34 -0600

    Prueba Tomar Fotografia v2

--- Fotograpp/app/src/main/java/com/example/fotograpp/MainActivity.java
+++ Fotograpp/app/src/main/java/com/example/fotograpp/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Método para tomar foto y crear el archivo
     static final int REQUEST_TAKE_PHOTO = 1;
-    private void tomarFoto(View view) {
+    public void tomarFoto(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
